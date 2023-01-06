e9ce7bfa3588684615309e0ae6a54cea26b9d22c
Haseeb Amer <h.amer@student.unsw.edu.au>
2023-01-06 17:35:23 +0500

    Initial Commit

---
+++ todolist/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  todolist
+//
+//  Created by Haseeb Amer on 6/1/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
